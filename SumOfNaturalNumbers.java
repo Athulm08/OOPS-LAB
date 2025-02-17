fefe953e40c899df53ad70af7d90e38691bbd7a8
Athulm08 <24mp2309@rit.ac.in>
2025-02-17 17:00:38 +0530

    Add files via upload

---
+++ SumOfNaturalNumbers.java
@@ -0,0 +1,19 @@
+import java . util . Scanner ;
+
+ public class SumOfNaturalNumbers {
+ public static void main ( String [] args ) {
+ Scanner scanner = new Scanner ( System . in ) ;
+ System . out . print (" Enter a positive integer : ") ;
+ int n = scanner . nextInt () ;
+
+ if ( n < 1) {
+ System . out . println (" Please enter a positive integer .") ;
+ } else {
+ int sum = n * ( n + 1) / 2;
+ System . out . println ("Sum of first " + n + " natural numbers
+is: " + sum ) ;
+ }
+
+ scanner . close () ;
+ }
+ }
